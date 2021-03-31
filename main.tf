6e40dbd6f4fa644b3a596dcd67c8e6d7e33067d1
raylaijh <32132864+raylaijh@users.noreply.github.com>
2021-03-31 11:40:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,39 +11,35 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example-1"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0f17ace83703225c5" # ap-southeast-1
+  ami           = data.aws_ami.ubuntu.id # ap-southeast-1
   instance_type = "t2.micro"
+  
+  tags = {
+    Name = "helloworld"
+    }
+  
 
   network_interface {
-    network_interface_id = aws_network_interface.foo.id
+    network_interface_id = eni-0e05f4c7fea01ca49 #hardcoded
     device_index         = 0
   }
 
