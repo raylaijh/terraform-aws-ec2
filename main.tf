ec16e82ee74eb40bdd5b5eba944b239357205f23
raylaijh <32132864+raylaijh@users.noreply.github.com>
2021-03-31 11:47:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "foo" {
   
 
   network_interface {
-    network_interface_id = eni-0e05f4c7fea01ca49 #hardcoded
+    network_interface_id = "eni-0e05f4c7fea01ca49" #hardcoded
     device_index         = 0
   }
 
